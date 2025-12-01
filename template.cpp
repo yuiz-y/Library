380b539586fa336b9a45c622c1b8690bc8bc7bea
yuiz-y <yui.mzn.z@gmail.com>
2025-12-01 23:25:17 +0900

    template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ld=long double;
+using vll=vector<ll>;
+using pll=pair<ll,ll>;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rrep(i,n) for(int i=n-1;0<=i;i--)
+#define REP(i,n) for(int i=1;i<=n;i++)
+#define all(a) a.begin(),a.end()
+#define sort(a) sort(all(a))
+#define rev(a) reverse(all(a))
+char el='\n';
+void YN(bool f){cout<<(f?"Yes":"No")<<"\n";}
+template<typename T> bool chmin(T& x,T y){if(x>y){x=y;return true;}return false;}
+template<typename T> bool chmax(T& x,T y){if(x<y){x=y;return true;}return false;}
+
+int main(){}
