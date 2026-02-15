04debc617a730c341a3518f0c3900d3205b0e534
yuiz-y <yui.mzn.z@gmail.com>
2026-02-16 08:27:51 +0900

    k<0 || n<k を追加

--- src/Combination.cpp
+++ src/Combination.cpp
@@ -1,8 +1,9 @@
 struct factorial{
     private:
-    vector<fp998t> a,b;
+    vector<fp998> a,b;
     public:
-    modint comb(ll n,ll k){
+    fp998 comb(ll n, ll k){
+        if(k<0 || n<k) return 0;
         return a[n]*b[n-k]*b[k];
     }
     factorial(int n){
@@ -10,6 +11,6 @@ struct factorial{
         b.resize(n+10,1);
         REP(i,n) a[i+1] = a[i]*(i+1);
         b[n+1] = 1/a[n+1];
-        for(int i=n;0<i;i--) b[i] = b[i+1]*(i+1);
+        for(int i=n; 0<i; i--) b[i] = b[i+1]*(i+1);
     }
 };
